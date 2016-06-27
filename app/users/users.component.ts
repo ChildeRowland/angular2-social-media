841be7bfa17b2b6ef93b2f2a4249a7e3c74afd2c
Christopher Mehmed <chrismehmed@gmail.com>
2016-06-27 13:17:58 -0400

    users service with observable

--- app/users/users.component.ts
+++ app/users/users.component.ts
@@ -1,14 +1,26 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+
+import { UsersService } from './users.service';
 
 @Component({
 	selector: 'users',
+	providers: [ UsersService ],
 	template: `
 		<div>
-			USERS
+			{{ users | json }}
 		</div>
 	`
 })
 
-export class UsersComponent {
+export class UsersComponent implements OnInit{
+	users;
 
+	constructor(private _usersService: UsersService) {
+
+	}
+
+	ngOnInit() {
+		this._usersService.getUsers()
+			.subscribe(data => this.users = data);
+	}
 }
